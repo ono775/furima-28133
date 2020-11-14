47175a43433002085a298e9f0da17079c8d32f36
ono775 <ono7751986@gmail.com>
2020-11-14 11:37:33 +0900

    Active Storage の導入
    has_one_attached を指定
    ストロングパラメータを指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price).merge(user_id: current_user.id)
+  end
+
 end
