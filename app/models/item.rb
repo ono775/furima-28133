5f8b8881bf658e803ca5ce654587c0f51db113af
ono775 <ono7751986@gmail.com>
2020-11-14 16:51:56 +0900

    category のバリデーションを設定
    カテゴリー選択のhtmlを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id
+    validates :shipping_charges_id
+    validates :shipping_area_id
+    validates :shipping_date_id
+    validates :price
+    validates :user_id
+  end
 end
