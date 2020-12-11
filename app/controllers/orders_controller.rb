3ecbd0836d85dcf3c84e5c23ef367886c36d2e80
ono775 <ono7751986@gmail.com>
2020-12-11 15:17:00 +0900

    購入ページへの遷移
    表記名の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,15 @@
 class OrdersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index, :create]
+
+  def index
+    @item = Item.find(params[:item_id])
+    if user_signed_in? && current_user.id != @item.user_id
+      @user_order = UserOrder.new
+    elsif user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def create
+  end
 end
