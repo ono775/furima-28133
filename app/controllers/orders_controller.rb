bfd26599fba0a03a2c4874dae4637afaed02f981
ono775 <ono7751986@gmail.com>
2020-12-12 18:18:06 +0900

    Form オブジェクトの作成
    User Order モデルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     if user_signed_in? && current_user.id != @item.user_id
       @user_order = UserOrder.new
     elsif user_signed_in? && current_user.id == @item.user_id
@@ -11,5 +11,25 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:user_order).permit(
+      :user_id, :item_id, :postal_code, :prefectures_id, :municipality,
+      :address, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item)
   end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,22 +1,23 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :user, :item, :postal_code :prefecture, :municipality, :address, :building_name, :phone_number, :order
+  attr_accessor :user_id, :item_id, :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number
 
   with_options presence: true do
-    validates :user
-    validates :item
+    validates :user_id
+    validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture, numericality: { other_than: 1 }
+    validates :prefectures_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
-    validates :phone_number, numericality: { only_integer: true, less_than: 12 }
-    validates :order
+    validates :phone_number, length: { maximum: 11 }
+    # validates :order_id
   end
 
   def save
-    user = User.create(user: user)
-    item = Item.create(item: item)
-    order = Order.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id.id)
+    Address.create(
+      postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality,
+      address: address, building_name: building_name, phone_number: phone_number, order_id: order.id
+    )
   end
 end
